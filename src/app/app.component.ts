39e591003fa8c1c995371ac0f98fbbf8a5568aea
augustodelisio <potasiopotaq@gmail.com>
2020-02-17 20:03:47 -0300

    segunda clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlaceholderService } from './services/placeholder.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-dream-app';
   unavarmas = 'Titulo viejo';
   nomboton = 'Boton viejo';
   valorDeInput = 'Inserte texto';
+  usuarios =[];
+  
+  constructor(private phService: PlaceholderService){}
+
+  async ngOnInit(){
+    //let usuarios = await this.phService.getUsers()
+    //console.log(usuarios[0])
+    //this.phService.getUsersAlt().subscribe(usu=>console.log(usu))
+    this.phService.getUsersAlt().subscribe(usu=>this.usuarios = usu)
+  }
+  
   cambiarTodo():void
   {
     this.cambiarContenido();
